64a0c3bedf96a17d21f158f25ff779d4ece2c367
kw283 <kw283@duke.edu>
2019-02-22 22:20:02 -0500

    add new helper class

--- proxyDaemon.cpp
+++ proxyDaemon.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "proxyDaemon.h"
+void proxymanager::runWithCache() {
+    proxyDaemon pd;
+    while(1) {
+        string requestContent = pd.acceptReq();
+        pd.createThread();
+        string methodstr, urlstr;
+        pd.parseReq(requestContent, methodstr, urlstr);
+
+        // 'GET' or 'POST'
+        if (methodstr == "GET") {
+            // ...
+            // create socket connected to target server
+            // send request
+            // recv response
+            // send back to client
+        }
+        else if(methodstr == "POST") {
+            // ...
+            // create socket connected to target server
+            // send request
+            // recv response
+        }
+        else {
+            // ...thread exception
+        }
+    }
+}
--- proxyDaemon.h
+++ proxyDaemon.h
@@ -8,6 +8,9 @@
 //define our request header for parse
 //
 class proxyDaemon {
+private:
+
+public:
     acceptReq(); // accept HTTP request from cline
     parseReq(); // parse HTTP request into method, URL and context
     conToServer(); // connect to required server
@@ -18,5 +21,10 @@ class proxyDaemon {
     responReq(); // response to client
 };
 
+class proxymanager{
+public:
+    void runWithCache(); // run program use cache
+    void runWithoutCache(); // run program without using cache
+};
 
 #endif //ERSS_HWK2_KW283_ZH89_PROXYDAEMON_H
